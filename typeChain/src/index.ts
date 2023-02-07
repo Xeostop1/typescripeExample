416f27fadc3aab4f2c0d7653ac747bdd6c22fb95
Hana Seo <97787658+Xeostop1@users.noreply.github.com>
2023-02-07 13:30:34 +0900

    tsconfig, package.json edit

---
+++ typeChain/src/index.ts
@@ -0,0 +1,17 @@
+const hello=()=>"hi";
+
+class Block{
+    constructor(
+        private data:string
+    ){}
+    static hello(){
+        return "🖐"
+    }
+}
+
+const time=new Date()
+time.getDate();
+console.log(`check`,time);
+
+
+//5.3 Declaration Files (14:43) 처음부터 시청
